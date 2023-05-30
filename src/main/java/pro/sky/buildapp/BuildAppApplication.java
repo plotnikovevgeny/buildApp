f6f88c8e4a26a7d5543189d3cfad1164b050f602
Plotnikov Eveny <plotnikovevgeny@hotmail.com>
2023-05-30 19:10:23 +0300

    Initial commit

---
+++ src/main/java/pro/sky/buildapp/BuildAppApplication.java
@@ -0,0 +1,13 @@
+package pro.sky.buildapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BuildAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BuildAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pro/sky/buildapp/BuildAppApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.buildapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BuildAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
